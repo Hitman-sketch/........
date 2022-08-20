f5191250e1e0b16909d42867db940cb757b8419e
Michael Attoh <52075079+Hitman-sketch@users.noreply.github.com>
2022-08-20 16:22:40 +0000

    Update codewar.py

--- codewar.py
+++ codewar.py
@@ -1,7 +1,7 @@
 import re
 def to_camel_case(text):
-    s = re.sub(r"[_|-]+", " ", text).title().replace(" ", "")
-    return ''.join([s[0].lower(), s[1:]])
+    format = re.sub(r"[_|-]+", " ", text).title().replace(" ", "")
+    return ''.join([format[0].lower(), format[1:]])
 
 
 print(to_camel_case('hello-world_it-is_me'))
